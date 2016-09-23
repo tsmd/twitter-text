ae7602e19166be0adbf5466aeb5041b6f734ebf8
Takayuki Shimada <shimada@necomesi.jp>
2016-09-23 21:55:34 +0900

    Passes tests

--- js/test/node_tests.js
+++ js/test/node_tests.js
@@ -7,7 +7,7 @@ assert.equal(typeof twttr, 'undefined', 'twttr is undefined before requiring twi
 
 var twitter = require('../twitter-text');
 
-assert.equal(typeof twitter.autoLink, 'function', 'twttr.txt is exported');
+assert.equal(typeof twitter.getTweetLength, 'function', 'twttr.txt is exported');
 assert.equal(typeof twttr, 'undefined', 'twttr is undefined after requiring twitter-text');
 assert.equal(typeof window, 'undefined', 'window is undefined after requiring twitter-text');
 
